8594a0b7ff70d05ec05e6356aed5cc7675ed91c7
石亀広大 <kira5052@gmail.com>
2018-12-31 11:35:59 +0900

    emotionの追加

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import Head from 'components/templates/head.js'
 import Navigation from 'components/templates/navigation'
-import css from 'static/css/pages/index.scss'
+import { jsx, css } from '@emotion/core'
 
+/** @jsx jsx */
 const Index = () => {
   return (
     <div>
       <Head title="Index page" />
       <Navigation />
-      <p className={css.item}>Hello world</p>
-      <p>Index</p>
+      <p
+        css={css`
+          color: #f00;
+        `}
+      >
+        String style
+      </p>
+      <p
+        css={{
+          backgroundColor: 'hotpink',
+          '&:hover': {
+            color: '#8a8'
+          }
+        }}
+      >
+        Object style
+      </p>
     </div>
   )
 }
