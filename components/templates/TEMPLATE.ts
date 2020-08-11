53c1f3d4fe4d1234126a506db096b7366ef4c804
koudaiishigame <kira5052@gmail.com>
2020-08-11 18:58:19 +0900

    TEMPLATEの変更

--- components/templates/TEMPLATE.ts
+++ components/templates/TEMPLATE.ts
@@ -7,12 +7,12 @@ type ContainerProps = {}
 type Props = {} & ContainerProps
 
 // (3) DOM層
-const Component: React.FC<Props> = props => ()
+export const Component: React.FC<Props> = props => ()
 
 // (4) Style層
 // const StyledComponent = styled(Component)``
 
 // (5) Container層
-// const Container: React.FC<ContainerProps> = props => {
+// export const Container: React.FC<ContainerProps> = props => {
 //   return <StyledComponent {...props} />
 // }
